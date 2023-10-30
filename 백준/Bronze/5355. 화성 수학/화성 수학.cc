8c5c4e0c14ef34af6898dc30f177c88bd6f0ad40
JinWook <105339077+jen454@users.noreply.github.com>
2023-10-30 12:07:08 +0900

    [Bronze II] Title: 화성 수학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5355. 화성 수학/화성 수학.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    int T;
+    cin>>T;
+    double n;
+    char ch;
+ 
+    for(int i=0; i<T; i++){
+    cin>>n;
+    while(1){
+        cin.get(ch);
+        if(ch=='\n'){
+            break;
+        }
+        if(ch=='@'){
+            n*=3;
+        }
+        if(ch=='%'){
+            n+=5;
+        }
+        if(ch=='#'){
+            n-=7;
+        }
+    
+    }
+    cout<<fixed;
+    cout.precision(2);
+    cout<<n<<endl;
+    }
+    return 0;
+}

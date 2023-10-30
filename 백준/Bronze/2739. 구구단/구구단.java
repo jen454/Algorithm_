1a4958e05d6219ad3ded3ec609c56dadaf27c923
JinWook <105339077+jen454@users.noreply.github.com>
2023-10-30 12:02:50 +0900

    [Bronze V] Title: 구구단, Time: 240 ms, Memory: 18524 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		for (int i=1; i<10; i++) {
+			System.out.println(N+" "+"*"+" "+i+" "+"="+" "+N*i);
+		}
+	}
+
+}

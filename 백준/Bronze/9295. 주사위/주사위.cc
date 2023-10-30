7c25a1b897a60d2b104c636d937d9ef1dc1d3a4e
JinWook <105339077+jen454@users.noreply.github.com>
2023-10-30 12:09:15 +0900

    [Bronze III] Title: 주사위, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int T,A,B;
+    cin >> T;
+    for (int i=1; i<T+1; i++) {
+        cin >> A >> B;
+        cout << "Case " << i << ": " << A+B << endl;
+    }
+    return 0;
+}

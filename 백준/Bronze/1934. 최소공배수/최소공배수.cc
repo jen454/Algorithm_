3ac3cf43e7d67842cc6119962b30ea57e3b70cf2
JinWook <105339077+jen454@users.noreply.github.com>
2023-10-30 11:58:27 +0900

    [Bronze I] Title: 최소공배수, Time: 48 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -0,0 +1,30 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int divide(int x, int y)
+{
+    if (x % y == 0)
+        return y;
+    else
+        return divide(y, x % y);
+}
+ 
+int main()
+{
+    int T;
+    int A, B;
+    cin >> T;
+ 
+    for (int i = 0; i < T; i++)
+    {
+        cin >> A >> B;
+        if (A >= B)
+        {
+            cout << A * B / divide(A, B) << endl;
+        }
+        else
+            cout << A * B / divide(B, A) << endl; 
+    }
+    return 0;
+}

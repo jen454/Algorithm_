3b3cde90d621044e7ab4d45ede01799bfbb2e7f2
JinWook <105339077+jen454@users.noreply.github.com>
2023-10-30 12:05:17 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 236 ms, Memory: 18368 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		int C = scanner.nextInt();
+		int D = scanner.nextInt();
+		int E = scanner.nextInt();
+		int F = scanner.nextInt();
+		System.out.print(1-A + " ");
+		System.out.print(1-B + " ");
+		System.out.print(2-C + " ");
+		System.out.print(2-D + " ");
+		System.out.print(2-E + " ");
+		System.out.print(8-F);
+	}
+
+}

4648894cb8de7edf2c739eb42cc41c82fbf2ddea
JinWook <105339077+jen454@users.noreply.github.com>
2023-10-30 12:07:23 +0900

    [Bronze IV] Title: 입실 관리, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cctype>
+#include <string>   
+ 
+using namespace std;
+ 
+int main() {
+ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    string s;
+    int n;
+    cin>>n;
+ 
+    for(int i=0; i<n; i++){
+ 
+        cin>>s;
+ 
+        for(int i=0; i<s.length(); i++){
+            if(isupper(s[i])){
+                s[i]=tolower(s[i]);
+            }
+        }
+            cout<<s<<"\n";
+    }
+}

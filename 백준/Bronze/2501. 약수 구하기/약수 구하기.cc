9bc6c133142fe3e1a91242d2bf59659d822f0415
JinWook <105339077+jen454@users.noreply.github.com>
2023-10-30 12:00:53 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N,K,cnt=0;
+    cin >> N >> K;
+    for (int i=1; i<=N; i++) {
+        if (N%i == 0) {
+            cnt++;
+        }
+        if (cnt == K) {
+            cout << i << endl;
+            break;
+        }
+    }
+    if (cnt != K) {
+        cout << 0 << endl;
+    }
+    return 0;
+}

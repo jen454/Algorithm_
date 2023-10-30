52168bdc11ce5b0336ef508cf94476ff4b3cb28b
JinWook <105339077+jen454@users.noreply.github.com>
2023-10-30 12:17:24 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int year = scanner.nextInt();
+		year -= 543;
+		System.out.println(year);
+	}
+}

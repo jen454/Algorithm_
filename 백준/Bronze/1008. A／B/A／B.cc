7f912427ab7a5740ebb616a85b2433b4bf03ae7c
JinWook <105339077+jen454@users.noreply.github.com>
2023-10-30 11:55:26 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double A,B;
+    cin >> A >> B;
+    cout.precision(12);
+    cout << fixed;
+    cout << A/B << endl;
+    return 0;
+}

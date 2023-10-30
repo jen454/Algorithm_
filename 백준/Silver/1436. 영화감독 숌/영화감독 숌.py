2ab52c6eafe2b2313d6b0dbd0b00b40f6619793b
JinWook <105339077+jen454@users.noreply.github.com>
2023-10-30 11:57:31 +0900

    [Silver V] Title: 영화감독 숌, Time: 548 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,10 @@
+N = int(input())
+movie = 666
+cnt = 0
+while (True):
+    if "666" in str(movie):
+        cnt += 1
+        if cnt == N:
+            print(movie)
+            break
+    movie += 1

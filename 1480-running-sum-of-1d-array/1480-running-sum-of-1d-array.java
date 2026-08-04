39e0c0a47232b771161ea49a9937e92daea04547
Shin JinWook <105339077+jen454@users.noreply.github.com>
2026-08-04 16:51:45 +0900

    Time: 0 ms (100%), Space: 44.1 MB (73.25%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,15 +1,12 @@
 class Solution {
     public int[] runningSum(int[] nums) {
         int[] answer = new int[nums.length];
+        answer[0] = nums[0];
 
-        for (int i=1; i<nums.length+1; i++) {
-            for (int j=0; j<i; j++) {
-                answer[i-1] += nums[j];
-            }
+        for (int i=1; i<nums.length; i++) {
+            answer[i] = nums[i] + answer[i-1];
         }
 
         return answer;
     }
 }
-
-// 0 01 012 0123

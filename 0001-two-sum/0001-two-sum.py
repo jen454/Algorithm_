dcce95bfd6116991fe87e67fcd63d543049e7345
Shin JinWook <105339077+jen454@users.noreply.github.com>
2026-08-23 17:47:36 +0900

    Time: 1936 ms (6.87%), Space: 19.8 MB (81.96%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        answer = []
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if (nums[i] + nums[j] == target):
+                    answer.append(i)
+                    answer.append(j)
+        return answer
+
+# 이중 포문 돌려서 조건 체크하기
